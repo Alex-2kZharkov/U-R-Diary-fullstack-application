b85de34c4644ac71d4c141542e75e5e45818d4ab
Alex-2kZharkov <Alexander.06.zharkov@gmail.com>
2020-06-29 19:22:46 +0600

    As always forgot to add files under git control

---
+++ client_side.orig/src/Components/Friends/Friend.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Friend() {
+  return <div></div>;
+}
+
+export default Friend;
---
+++ client_side.orig/src/Components/Friends/Friends.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import PersonalRoomHeader from '../PersonalRoomHeader';
+import css from './Friends.module.css';
+export class Friends extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      requireNickname: '',
+    };
+  }
+  componentDidMount() {
+    this.props.setUserNickname(this.props.match.params.id);
+  }
+  render() {
+    return (
+      <div className={css.container}>
+        <PersonalRoomHeader
+          user={this.props.userNickname}
+          id={this.props.match.params.id}
+        />
+        <div className={css.wrap}>
+          <form action='' autoComplete='on'>
+            <input
+              name='search'
+              type='text'
+              placeholder='Who are we looking for ?'
+              className={css.search_field}
+              value={this.state.requireNickname}
+              onChange={(event) =>
+                this.setState({
+                  requireNickname: event.target.value,
+                })
+              }
+            />{' '}
+            <button
+              type='button'
+              className={`${css.submit} ${css.search_modif}`}
+            >
+              <i className={`fas fa-search ${css.search_icon}`}></i>
+            </button>
+            <button
+              onClick={(event) =>
+                this.setState({
+                  requireNickname: '',
+                })
+              }
+              type='button'
+              className={`${css.submit} ${css.cancel_modification}`}
+            >
+              <i className={`fas fa-times ${css.search_icon}`}></i>
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Friends;
